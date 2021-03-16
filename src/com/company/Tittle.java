15a9d5dfb6f69bee5fdf747a8a8a3e520aa0a689
Your Name <you@example.com>
2021-03-16 20:05:56 +0100

    -

--- src/com/company/Tittle.java
+++ src/com/company/Tittle.java
@@ -2,9 +2,9 @@ package com.company;
 
 public class Tittle {
 
-    void mostrar(String titulo){
+    void mostrar(String tittle){
         System.out.println("+-----------------+");
-        System.out.println("|  " + titulo +"  |");
+        System.out.println("|  " + tittle +"  |");
         System.out.println("+-----------------+");
     }
 
